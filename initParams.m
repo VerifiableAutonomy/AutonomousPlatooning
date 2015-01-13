d297d65b0fa24b484bea1b935e1673d42db4fab1
Owen McAree <o.mcaree@sheffield.ac.uk>
2015-01-13 16:32:45 +0000

    Added Full V2V follower control
    - Implemented V2V based steering for follower using sliding mode approach
      similar to longitudinal control (with additional leader-error term)
    - Added global sensor data (lateralError and heading), which can be used
      when not obscured by vehicle ahead (but this check is currently disabled
      for debugging, so these measures are always available)
    - Added very very simple Stateflow chart to handle transition from
      not-platooning to platooning

--- initParams.m
+++ initParams.m
@@ -1,2 +1,9 @@
-mainSampleTime = 0.02;      % Must match TORCS
-controlSampleTime = 0.04;   % Must be less than (and an integer multiple of) mainSampleTime
+sampleTimes.main = 0.02;      % Must match TORCS
+sampleTimes.control = 0.02;   % Must be less than (and an integer multiple of) mainSampleTime
+sampleTimes.sensor = 0.04;
+sampleTimes.v2v = 0.1;
+
+delays.v2v = 0.02;
+delays.sensor = 0.2;
+
+spacing = 6;
