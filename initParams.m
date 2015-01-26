26792840048bcc6823508fbe2594d7fe403bdffc
Owen McAree <o.mcaree@sheffield.ac.uk>
2015-01-26 13:28:06 +0000

    Merge bugfixes from experiment branch

--- initParams.m
+++ initParams.m
@@ -3,7 +3,7 @@ sampleTimes.control = 0.02;   % Must be less than (and an integer multiple of) m
 sampleTimes.sensor = 0.04;
 sampleTimes.v2v = 0.1;
 
-delays.v2v = 0.02;
+delays.v2v = 0.01;
 delays.sensor = 0.2;
 
 spacing = 6;
